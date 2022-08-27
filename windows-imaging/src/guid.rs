aba31e6265fa4e5b675b63329cc7b3aa010f3171
Mona <656889+monax3@users.noreply.github.com>
2022-08-27 12:18:00 +0100

    rewrite: separated out windows-imaging WIC crate entirely to write RGB PNGs

---
+++ windows-imaging/src/guid.rs
@@ -0,0 +1,71 @@
+use windows::core::GUID;
+use windows::Win32::Graphics::Imaging::{
+    GUID_ContainerFormatPng, GUID_WICPixelFormat24bppBGR, GUID_WICPixelFormat32bppRGBA,
+};
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum PixelFormat {
+    RGBA32bpp,
+    BGRA24bpp,
+}
+
+impl PixelFormat {
+    pub fn bpp(self) -> u32 {
+        match self {
+            PixelFormat::RGBA32bpp => 4,
+            PixelFormat::BGRA24bpp => 3,
+        }
+    }
+
+    pub fn as_guid(self) -> &'static GUID {
+        self.into()
+    }
+
+    pub fn from_guid(guid: &GUID) -> Option<Self> {
+        match guid {
+            guid if guid == &GUID_WICPixelFormat24bppBGR => Some(Self::BGRA24bpp),
+            guid if guid == &GUID_WICPixelFormat32bppRGBA => Some(Self::RGBA32bpp),
+            _ => None,
+        }
+    }
+}
+
+impl From<PixelFormat> for &'static GUID {
+    fn from(format: PixelFormat) -> &'static GUID {
+        match format {
+            PixelFormat::RGBA32bpp => &GUID_WICPixelFormat32bppRGBA,
+            PixelFormat::BGRA24bpp => &GUID_WICPixelFormat24bppBGR,
+        }
+    }
+}
+
+pub enum Container {
+    Png,
+}
+
+impl Container {
+    pub fn extension(self) -> &'static str {
+        match self {
+            Container::Png => "png",
+        }
+    }
+
+    pub fn as_guid(self) -> &'static GUID {
+        self.into()
+    }
+
+    pub fn from_guid(guid: &GUID) -> Option<Self> {
+        match guid {
+            guid if guid == &GUID_ContainerFormatPng => Some(Self::Png),
+            _ => None,
+        }
+    }
+}
+
+impl From<Container> for &'static GUID {
+    fn from(container: Container) -> &'static GUID {
+        match container {
+            Container::Png => &GUID_ContainerFormatPng,
+        }
+    }
+}
---
+++ windows-imaging/src/lib.rs
@@ -0,0 +1,209 @@
+//! FIXME: Assumes stride = width * bpp
+//! TODO: test what happens if the factory gets dropped
+//! TODO: use enums instead of GUIDs
+
+use std::mem::MaybeUninit;
+
+use windows::core::{Result, HSTRING};
+use windows::Win32::Graphics::Imaging::D2D::IWICImagingFactory2;
+use windows::Win32::Graphics::Imaging::{
+    CLSID_WICImagingFactory, CLSID_WICImagingFactory2, IWICBitmap, IWICBitmapSource,
+    IWICFormatConverter, IWICImagingFactory, WICBitmapDitherTypeNone, WICBitmapEncoderNoCache,
+    WICBitmapPaletteTypeMedianCut, WICRect,
+};
+use windows::Win32::System::Com::{CoCreateInstance, CLSCTX_INPROC_SERVER};
+use windows::Win32::System::SystemServices::GENERIC_WRITE;
+
+mod guid;
+pub use guid::{Container, PixelFormat};
+pub mod prelude {
+    pub use super::Bitmap;
+}
+
+pub fn wic() -> Result<IWICImagingFactory> {
+    unsafe { CoCreateInstance(&CLSID_WICImagingFactory, None, CLSCTX_INPROC_SERVER) }
+}
+
+pub fn wic2() -> Result<IWICImagingFactory2> {
+    unsafe { CoCreateInstance(&CLSID_WICImagingFactory2, None, CLSCTX_INPROC_SERVER) }
+}
+
+pub trait AsWICImagingFactory {
+    fn as_wic_factory<'a>(&'a self) -> &'a IWICImagingFactory;
+}
+
+impl<T> AsWICImagingFactory for T
+where
+    for<'a> &'a IWICImagingFactory: From<&'a T>,
+{
+    fn as_wic_factory<'a>(&'a self) -> &'a IWICImagingFactory {
+        self.into()
+    }
+}
+
+pub fn bitmap_from_memory(
+    factory: impl AsWICImagingFactory,
+    format: PixelFormat,
+    width: u32,
+    height: u32,
+    stride: u32,
+    data: &[u8],
+) -> Result<IWICBitmap> {
+    let factory = factory.as_wic_factory();
+
+    unsafe {
+        factory.CreateBitmapFromMemory(width as u32, height as u32, format.as_guid(), stride, data)
+    }
+}
+
+// pub fn bitmap_from_directxtex(&self, image: &DXImage, array_index: usize) -> Result<WICSource> {
+//     const RGBA_BPP: u32 = 4;
+
+//     let image = image.to_rgba()?;
+//     let metadata = image.metadata()?;
+//     let buf = image.image(array_index)?;
+
+//     let bitmap = unsafe {
+//         self.0.CreateBitmapFromMemory(
+//             metadata.width as u32,
+//             metadata.height as u32,
+//             &GUID_WICPixelFormat32bppRGBA,
+//             metadata.width as u32 * RGBA_BPP,
+//             &buf,
+//         )
+//     }?;
+
+//     Ok(WICSource {
+//         wic:   self.0.clone(),
+//         inner: bitmap.cast()?,
+//     })
+// }
+
+pub trait Bitmap {
+    fn as_wic_bitmap(&self) -> &IWICBitmap;
+
+    fn pixel_format(&self) -> Result<PixelFormat> {
+        unsafe { self.as_wic_bitmap().GetPixelFormat() }
+            .map(|guid| PixelFormat::from_guid(&guid).unwrap_or_else(|| unimplemented!()))
+    }
+}
+
+impl<T> Bitmap for T
+where
+    for<'a> &'a IWICBitmap: From<&'a T>,
+{
+    fn as_wic_bitmap(&self) -> &IWICBitmap {
+        self.into()
+    }
+}
+
+pub trait BitmapSource {
+    fn as_wic_bitmap_source(&self) -> &IWICBitmapSource;
+
+    fn rect(&self) -> Result<WICRect> {
+        let mut width = 0;
+        let mut height = 0;
+
+        unsafe { self.as_wic_bitmap_source().GetSize(&mut width, &mut height) }?;
+
+        // FIXME: better error code
+        Ok(WICRect {
+            X: 0,
+            Y: 0,
+            Width: width.try_into().unwrap_or(i32::MAX),
+            Height: width.try_into().unwrap_or(i32::MAX),
+        })
+    }
+
+    fn convert_to_pixel_format(
+        &self,
+        factory: impl AsWICImagingFactory,
+        to: PixelFormat,
+    ) -> Result<IWICFormatConverter> {
+        let converter = unsafe { factory.as_wic_factory().CreateFormatConverter() }?;
+
+        // FIXME: options?
+        unsafe {
+            converter.Initialize(
+                self.as_wic_bitmap_source(),
+                to.as_guid(),
+                WICBitmapDitherTypeNone,
+                None,
+                0.0,
+                WICBitmapPaletteTypeMedianCut,
+            )
+        }?;
+
+        Ok(converter)
+    }
+
+    fn save<'a>(
+        &self,
+        factory: impl AsWICImagingFactory,
+        file_name: impl Into<HSTRING>,
+        container: Container,
+    ) -> Result<()> {
+        let factory = factory.as_wic_factory();
+        let file_name = file_name.into();
+
+        let stream = unsafe {
+            let stream = factory.CreateStream()?;
+            stream.InitializeFromFilename(&file_name, GENERIC_WRITE)?;
+            stream
+        };
+
+        let encoder = unsafe {
+            let encoder = factory.CreateEncoder(container.as_guid(), std::ptr::null())?;
+            encoder.Initialize(&stream, WICBitmapEncoderNoCache)?;
+            encoder
+        };
+
+        let frame = unsafe {
+            let mut frame = MaybeUninit::uninit();
+            encoder.CreateNewFrame(frame.as_mut_ptr(), std::ptr::null_mut())?;
+            let frame = frame.assume_init().unwrap();
+            frame.Initialize(None)?;
+            frame
+        };
+
+        let rect = self.rect()?;
+
+        unsafe {
+            frame.WriteSource(self.as_wic_bitmap_source(), &rect)?;
+            frame.Commit()?;
+            encoder.Commit()?;
+        }
+        Ok(())
+    }
+    }
+
+impl<T> BitmapSource for T
+where
+    for<'a> &'a IWICBitmapSource: From<&'a T>,
+{
+    fn as_wic_bitmap_source(&self) -> &IWICBitmapSource {
+        self.into()
+    }
+}
+
+#[cfg(disabled)]
+#[test]
+fn test_rgb() {
+    const FROM: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/testdata/rgb.png");
+    const TO: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/testdata/rgb_wic_test.png");
+
+    crate::util::log_for_tests(true);
+
+    let dx = DXImage::load(FROM).expect("DXImage::load");
+    let wic = WIC::new().expect("WIC::new");
+    let bitmap = wic
+        .bitmap_from_directxtex(&dx, 0)
+        .expect("WIC::bitmap_from_directxtex");
+    let from_pixel_format = bitmap.pixel_format().expect("WICSource::pixel_format");
+
+    let rgb = bitmap
+        .to_pixel_format(&from_pixel_format, PIXEL_FORMAT_BGR)
+        .expect("WICSource::to_pixel_format");
+
+    rgb.save(TO, CONTAINER_PNG).expect("WICBitmap::save");
+}
